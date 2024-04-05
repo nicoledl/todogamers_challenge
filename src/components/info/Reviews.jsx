612a890e14f75ff0fc473663bd381067842ea67d
Nicole <nicolelosana@gmail.com>
2024-04-05 05:05:21 -0300

    Se agregó un collapse para ver la reseña completa

--- src/components/info/Reviews.jsx
+++ src/components/info/Reviews.jsx
@@ -1,9 +1,15 @@
 'use client'
 
-import { Avatar, Box, Grid, GridItem, ScaleFade, Text, useDisclosure } from '@chakra-ui/react';
+import { Avatar, Box, Grid, GridItem, ScaleFade, Text, Collapse } from '@chakra-ui/react';
 import reviews from '../../database/reviews.JSON';
+import { useState } from 'react';
 
 export default function Reviews() {
+  const [show, setShow] = useState({})
+
+  const handleToggle = (id) => {
+    setShow(prevState => ({ ...prevState, [id]: !prevState[id] }));
+  }
 
   return (
     <>
@@ -11,9 +17,9 @@ export default function Reviews() {
       <Box>
         {
           reviews.map((elem, id)=>{
-          const {name, review, img_url} = elem
-
-          return(
+            const {name, review, img_url} = elem
+            
+            return(
           <ScaleFade key={id} initialScale={0.2} in={true} transition={{ enter: {delay: id/2}}}>
             <Grid
             h='min-content'
@@ -22,7 +28,10 @@ export default function Reviews() {
             >
               <GridItem colSpan={2} display='flex' alignItems='center' gap={2} justifyContent='start'><Avatar size='md' src={img_url}/><Text as='b' fontSize='md'>{name}</Text></GridItem>
               <GridItem colSpan={1} display='flex' alignItems='center' justifyContent='end'><Text fontSize='xs' color='#e11ad6'>responder</Text></GridItem>
-              <GridItem h='max-content' colSpan={3} display='flex' alignItems='center'><Text fontSize='sm'>{review}</Text></GridItem>
+              <GridItem h='max-content' colSpan={3} display='flex' alignItems='center'>
+                <Collapse startingHeight={50} in={show[id]} onClick={() => handleToggle(id)}>
+                  {show[id] ? review : review.substring(0, 50) + '...' }</Collapse>
+              </GridItem>
             </Grid>
           </ScaleFade>
           )
